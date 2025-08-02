6b07285c812cf03e316ca11a78485dfefd181cc2
Connor Valle <81936436+connorv52@users.noreply.github.com>
2025-08-01 18:40:16 -0700

    Add market_reviewtext_whitespace_sentiment.sql

--- sql/market_reviewtext_whitespace_sentiment.sql
+++ sql/market_reviewtext_whitespace_sentiment.sql
@@ -4,7 +4,7 @@ FROM dbo.customer_reviews -- Table containing reviews given by customers on thei
 -- Easily noticed that the ReviewText column contains double spacing; can be fixed with the REPLACE function
 -- ***********************
 -- ***********************
-
+	
 -- Query to clean whitespace issues in the ReviewText column
 SELECT
 	ReviewID,
@@ -16,4 +16,5 @@ SELECT
 FROM
 	dbo.customer_reviews;
 	
+
 -- Will perform sentiment analysis on the ReviewText column with Python packages
