420352c5062c33ead45d95a3abc2a633136aaa6f
Connor Valle <81936436+connorv52@users.noreply.github.com>
2025-08-01 18:38:26 -0700

    Add market_formatting_and_standardizing

---
+++ sql/market_formatting_and_standardizing.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM dbo.engagement_data -- Table containing engagement data (likes) on the marketing content types (e.g., Blog or Video)
+
+-- Quick glance at the table shows some obvious formatting issues (e.g., zeros, casing, spacing)
+-- ***********************
+-- ***********************
+
+SELECT
+	EngagementID,
+	ContentID,
+	CampaignID,
+	ProductID, -- Selecting the various IDs first
+	UPPER(REPLACE(ContentType, 'Socialmedia', 'Social Media')) AS ContentType, -- Replaces 'Socialmedia' with 'SocialMedia' and capitalizes all other content types
+	LEFT(ViewsClicksCombined, CHARINDEX('-', ViewsClicksCombined) - 1) AS Views, -- Left part of the ViewsClicksCombined column is the total Views
+	RIGHT(ViewsClicksCombined, LEN(ViewsClicksCombined) - CHARINDEX('-', ViewsClicksCombined)) AS Clicks, -- Right part of the ViewsClicksCombined column is the total Clicks
+	Likes,
+	FORMAT(CONVERT(DATE, EngagementDate), 'MM/dd/yyyy') AS EngagementDate -- Converts and formats the date as mm.dd.yyyy
+FROM
+	dbo.engagement_data;
